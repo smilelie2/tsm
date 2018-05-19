d2ec74ed36775b1690b9094385a0a8574a4c7818
Piyachok Duangthongsuk <piyachok.d@ku.th>
2018-05-19 23:47:30 +0700

    add gitignore

--- resources/views/assessor/workStatistic.blade.php
+++ resources/views/assessor/workStatistic.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">Dashboard {{ "(Year school : " }} {{ $yearschool.")" }}</div>
 
@@ -26,28 +26,6 @@
                         </div>
                     </div>
                         <br />
-                    <form method="GET" action="{{ route('assessor/nisitStatistic') }}">
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nisit name') }}</label>
-
-                            <div class="col-md-5">
-                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}"  autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                            <div class="form-group row mb-0">
-                                <div class="col-md-5 offset-md-5">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Search') }}
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
 
                         <table border="1">
                             <tr><td>#</td><td>Name</td><td>created_date</td><td>due_time</td><td>info</td><td>year_school</td><td>patron</td><td>status</td>
