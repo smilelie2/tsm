1db415bfd90780f2d8e553af266aabfd096eed2c
prem <natthapong.tan@damrong.ac.th>
2018-05-21 03:50:23 +0700

    edit assessor view and query abit

--- resources/views/assessor/manage.blade.php
+++ resources/views/assessor/manage.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-12">
+        <div class="col-md-9">
             <div class="card">
                 <div class="card-header"> {{ __('Manage')}}  {{ "(Year school : " }} {{ $yearschool.")" }}</div>
 
