23c3c6947930c3a094669fbe5dcf2c52b15f85fa
Piyachok Duangthongsuk <piyachok.d@ku.th>
2018-05-21 04:39:27 +0700

    not change

--- resources/views/assessor/manage.blade.php
+++ resources/views/assessor/manage.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-9">
+        <div class="col-md-11">
             <div class="card">
                 <div class="card-header"> {{ __('Manage')}}  {{ "(Year school : " }} {{ $yearschool.")" }}</div>
 
